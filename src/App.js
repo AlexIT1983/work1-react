7f5f95c36353a534136666bb1068e9b1f8c98d62
Алекс Антипов <alex@MacBook-Air-Aleks.local>
2024-01-21 00:24:21 +0300

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import logo from './logo.svg'; // декларативный
 import './App.css'; // декларативный
+import { createElement } from 'react';
 const dateYear = new Date().getFullYear(); // императивный
 
 // сама функция императивный подход
 export const App = () => {
 	return (
 		// код ниже декларативный (мы указываем что хотим получить)
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload 3.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>Year: {dateYear}.</p>
-			</header>
-		</div>
+		createElement(
+			'div',
+			{ className: 'App' },
+			createElement(
+				'header',
+				{ className: 'App-header' },
+				createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+				createElement(
+					'p',
+					null,
+					'Edit ',
+					createElement('code', null, 'src/App.js'),
+					' and save to reload 3.',
+				),
+				createElement(
+					'a',
+					{
+						className: 'App-link',
+						href: 'https://reactjs.org',
+						target: '_blank',
+						rel: 'noopener noreferrer',
+					},
+					'Learn React',
+				),
+				createElement('p', null, `Year: ${dateYear}`),
+			),
+		)
 	);
 };
